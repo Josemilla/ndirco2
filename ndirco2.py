c989def29ea97d8c46b47d558593ac84ba8ba99e
Josema <josemalive@gmail.com>
2021-03-14 21:24:55 +0100

    Añadido detecion de dia/noche para el briilo de la pantalla

--- ndirco2.py
+++ ndirco2.py
@@ -2,14 +2,19 @@
 # Otro script para mostrar los datos del sensot MH-Z14A
 # 2021 Josema - josemalive@gmail.com
 
+# pip install ephem
+
 import time
 import serial
-# import fourletterphat
 import scrollphathd
 from scrollphathd.fonts import font3x5
+import ephem
+from datetime import datetime as dt
+from datetime import timedelta
+import math
 
 # Comando para leer la concentración de CO2
-PETICION = [0xff, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79]
+PETICION = [0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79]
 # Rango1 de 0 a 2000 ppm
 RANGO1 = [0xFF, 0x01, 0x99, 0x00, 0x00, 0x00, 0x07, 0xd0, 0x8F]
 # Rango2 de 0 a 5000 ppm
@@ -25,7 +30,7 @@ DES_AUTO_CALIBRACION = [0xFF, 0x01, 0x79, 0x00, 0x00, 0x00, 0x00, 0x00, 0x86]
 
 MAXIMO_BARRA = 1000
 MINIMO_BARRA = 400
-BRILLO = 0.3
+BRILLO = None
 
 # Configuramos la conexión serie según los datos del fabricante
 sensor = serial.Serial(
@@ -35,6 +40,8 @@ sensor = serial.Serial(
         stopbits = serial.STOPBITS_ONE,
         bytesize = serial.EIGHTBITS,
         timeout = 1)
+
+scrollphathd.rotate(180)
 
 def imprime_scrollphat(dato):
     global BRILLO
@@ -47,11 +54,11 @@ def imprime_scrollphat(dato):
         x = 5
 
     scrollphathd.clear()
-    scrollphathd.write_string(str(dato), x=x, y=0, font=font3x5, brightness = BRILLO)
-    scrollphathd.fill(BRILLO - 0.1 if BRILLO > 0.1 else BRILLO, 0, 6, int((dato - 400) / ((MAXIMO_BARRA - MINIMO_BARRA) / 17)), 1)
-    scrollphathd.fill(BRILLO - 0.1 if BRILLO > 0.1 else BRILLO, 0, 5, 1, 2)
-    scrollphathd.fill(BRILLO - 0.1 if BRILLO > 0.1 else BRILLO, 8, 5, 1, 2)
-    scrollphathd.fill(BRILLO - 0.1 if BRILLO > 0.1 else BRILLO, 16, 5, 1, 2)
+    scrollphathd.write_string(str(dato), x = x, y = 1, font = font3x5, brightness = BRILLO)
+    # scrollphathd.fill(BRILLO - 0.1 if BRILLO > 0.1 else BRILLO, 0, 6, int((dato - 400) / ((MAXIMO_BARRA - MINIMO_BARRA) / 17)), 1)
+    # scrollphathd.fill(BRILLO - 0.1 if BRILLO > 0.1 else BRILLO, 0, 5, 1, 2)
+    # scrollphathd.fill(BRILLO - 0.1 if BRILLO > 0.1 else BRILLO, 8, 5, 1, 2)
+    # scrollphathd.fill(BRILLO - 0.1 if BRILLO > 0.1 else BRILLO, 16, 5, 1, 2)
     scrollphathd.show()
 
 def obten_co2():
@@ -62,14 +69,39 @@ def obten_co2():
         # El valor que buscamos se encuentra en el byte 2 (high byte) y 3 (low byte).
         return (respuesta[2] << 8) | respuesta[3]
 
+def ajustar_brillo():
+    global BRILLO
+
+    sol = ephem.Sun()
+    observador = ephem.Observer()
+    # ↓ Define your coordinates here ↓
+    observador.lat, observador.lon, observador.elevation = '40.285408', '-3.788855', 660
+    # ↓ Set the time (UTC) here ↓
+    observador.date = dt.utcnow()
+    sol.compute(observador)
+    # altitud_sol = sol.alt
+    # print(altitud_sol*180/math.pi)
+    # -16.8798870431°
+    angulo = (sol.alt * 180 / math.pi)
+    if  angulo > 0: # Es de día
+        BRILLO = 0.3
+    else: # Es de noche
+        BRILLO = 0.1
+    # print (angulo)
+
 # Configuramos el sensor en el rango de medición de 0 - 2000 ppm. Cuanto más bajo es el rango, mejor es la precición.
-sensor.write(bytearray(RANGO1))
+sensor.write(bytearray(RANGO2))
 
 # Por experiencia, el primer valor devuelto por el sensor es una medida errónea. Así que leemos y descartamos el valor.
 obten_co2()
 time.sleep(1)
+
 # Volvemos a hacer un a lectura para mostrar el primer valor en la pantalla
 valor_co2_anterior = obten_co2()
+
+hora_comprobacion_luz = dt.now()
+ajustar_brillo()
+
 imprime_scrollphat(valor_co2_anterior)
 
 # Entramos el bucle y no salimos nunca
@@ -92,3 +124,8 @@ while True:
         if abs(valor_co2_anterior - valor_co2) <= 10:
             time.sleep(0.3)
     valor_co2_anterior = valor_co2
+
+    if dt.now() >= (hora_comprobacion_luz + timedelta(minutes=1)):
+        # print("Sólo entro aqui cada minuto")
+        ajustar_brillo()
+        hora_comprobacion_luz = dt.now()
