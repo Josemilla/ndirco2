75b7e710197bc763db7e7833e0402f826e60cc3e
Josema <josemalive@gmail.com>
2021-04-01 16:41:18 +0200

    Ahora se controla Control+C

--- ndirco2.py
+++ ndirco2.py
@@ -12,11 +12,11 @@
 import time
 import serial
 import scrollphathd
+import math
+import ephem
 from scrollphathd.fonts import font3x5
 from datetime import datetime as dt
 from datetime import timedelta
-import math
-import ephem
 
 # Comando para leer la concentración de CO2
 PETICION = [0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79]
@@ -33,8 +33,8 @@ ACT_AUTO_CALIBRACION = [0xFF, 0x01, 0x79, 0xA0, 0x00, 0x00, 0x00, 0x00, 0xE6]
 # Desactivar auto calibración
 DES_AUTO_CALIBRACION = [0xFF, 0x01, 0x79, 0x00, 0x00, 0x00, 0x00, 0x00, 0x86]
 
-MAXIMO_BARRA = 800
-MINIMO_BARRA = 400
+# MAXIMO_BARRA = 800
+# MINIMO_BARRA = 400
 BRILLO = None
 
 # Configurar aquí los datos de longitud, latitud y altura
@@ -80,9 +80,10 @@ def obten_co2():
     # Enviamos el comando para pedir el valor de CO2
     sensor.write(bytearray(PETICION))
     respuesta = sensor.read(9)
-    if len(respuesta) == 9:
+    # if len(respuesta) == 9:
         # El valor que buscamos se encuentra en el byte 2 (high byte) y 3 (low byte).
-        return (respuesta[2] << 8) | respuesta[3]
+        # return (respuesta[2] << 8) | respuesta[3]
+    return (respuesta[2] << 8) | respuesta[3]
 
 # Esta funcion usa la librería ephem para calcular si es de día en función de los datos de longitud y latitud y ajusta la variable BRILLO
 def ajustar_brillo():
@@ -108,16 +109,14 @@ def ajustar_brillo():
             BRILLO = 0.3
         else: # Es de noche
             BRILLO = 0.1
-        # print (angulo)
-    # ...si no ponemos el brillo a 0.3
+    # ...si no ponemos el brillo a 0.2
     else:
-        BRILLO = 0.3
+        BRILLO = 0.2
 
-# Mostramos mensaje de información en consola
-print("ndirCO2.py v1.0 - Josema - 30 de marzo de 2021 - josemalive@gmail.com\n")
 hora_comprobacion_luz = dt.now()
 ajustar_brillo()
-scrollphathd.clear()
+
+# Mostramos la palabra HEAT (no hay palabra en español de cuatro letras) en la pantalla durante los tres minutos de calentamiento
 scrollphathd.write_string("HEAT", x = 1, y = 1, font = font3x5, brightness = BRILLO)
 scrollphathd.show()
 
@@ -128,42 +127,50 @@ sensor.write(bytearray(RANGO1))
 obten_co2()
 
 # Esperamos tres minutos, tiempo que indica el fabricante para el calentamiento del sensor. El for muestra la cuenta atrás.
+print("\nndirCO2.py v1.0 - Josema - 30 de marzo de 2021 - josemalive@gmail.com\n")
 print("Esperando al calentamiento del sensor (Control + C para saltar)...")
 try:
     for segundos in range(180, 0, -1):
-        print(" " + str(segundos) + " segundos.  ", end="\r")
+        print("  " + str(segundos) + " segundos.  " if segundos > 1 else " segundo.   ", end="\r")
         time.sleep(1)
+    print("Iniciando...")
 except KeyboardInterrupt:
     pass
+
+# Vuelvo a pedir el valor de CO2 para intentar evitar el valor 3420 que devuelve al principio
+obten_co2()
 
 # Volvemos a hacer un a lectura para mostrar el primer valor en la pantalla
 valor_co2_anterior = obten_co2()
 imprime_scrollphat(valor_co2_anterior)
 
 # Entramos el bucle y no salimos nunca
-while True:
-    # Paramos un segundo en cada iteración del bucle
-    time.sleep(1)
-    valor_co2 = obten_co2()
-    # Calculamos la dirección de bucle for
-    if valor_co2 > valor_co2_anterior:
-        direccion_for = 1
-    elif valor_co2 < valor_co2_anterior:
-        direccion_for = -1
-    else:
-        imprime_scrollphat(valor_co2)
-        continue
-    
-    # Este for muestra la animación del conteo cuando cambia el valor
-    for digito in range(valor_co2_anterior, valor_co2, direccion_for):
-        imprime_scrollphat(digito)
-        # Sólo si el salto entre valores es menor de 15 hacemos una pausa de 300ms. Si no lo fuera no hacemos pausa para que la animación no sea tediosa.
-        if abs(valor_co2_anterior - valor_co2) <= 15:
-            time.sleep(0.3)
-    valor_co2_anterior = valor_co2
+try:
+    while True:
+        # Paramos un segundo en cada iteración del bucle
+        time.sleep(1)
+        valor_co2 = obten_co2()
+        # Calculamos la dirección de bucle for
+        if valor_co2 > valor_co2_anterior:
+            direccion_for = 1
+        elif valor_co2 < valor_co2_anterior:
+            direccion_for = -1
+        else:
+            imprime_scrollphat(valor_co2)
+            continue
+        
+        # Este for muestra la animación del conteo cuando cambia el valor
+        for digito in range(valor_co2_anterior, valor_co2, direccion_for):
+            imprime_scrollphat(digito)
+            # Sólo si el salto entre valores es menor de 15 hacemos una pausa de 300ms. Si no lo fuera no hacemos pausa para que la animación no sea tediosa.
+            if abs(valor_co2_anterior - valor_co2) <= 15:
+                time.sleep(0.3)
+        valor_co2_anterior = valor_co2
 
-    # Entramos cada minuto aquí para comprobar si es de día o de noche
-    if dt.now() >= (hora_comprobacion_luz + timedelta(minutes=1)):
-        # print("Sólo entro aquí cada minuto")
-        ajustar_brillo()
-        hora_comprobacion_luz = dt.now()
+        # Entramos cada minuto aquí para comprobar si es de día o de noche
+        if dt.now() >= (hora_comprobacion_luz + timedelta(minutes=1)):
+            # print("Sólo entro aquí cada minuto")
+            ajustar_brillo()
+            hora_comprobacion_luz = dt.now()
+except KeyboardInterrupt:
+    scrollphathd.clear()
